5ce269ef2b15e6da8e0a6f68b4c14dd09af101dc
EveretteLi <everetteli12@gmail.com>
2023-04-02 14:57:04 -0700

    change data set

--- main.js
+++ main.js
@@ -117,7 +117,7 @@ require([
 
   // define the earthquakes layer
   const earthquakeLayer = new CSVLayer({
-    url: "./earthquake_data.csv",
+    url: "./eq_replace.csv",
     elevationInfo: exaggeratedElevation,
     screenSizePerspectiveEnabled: false,
     renderer: {
