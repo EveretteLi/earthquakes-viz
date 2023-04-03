5bac61e493b1374be4cfe1e4e97d73009e92fa75
EveretteLi <everetteli12@gmail.com>
2023-04-02 23:17:52 -0700

    use less image

--- main.js
+++ main.js
@@ -117,7 +117,7 @@ require([
 
   // define the earthquakes layer
   const earthquakeLayer = new CSVLayer({
-    url: "./eq_image.csv",
+    url: "./eq_image_lite.csv",
     elevationInfo: exaggeratedElevation,
     screenSizePerspectiveEnabled: false,
     renderer: {
